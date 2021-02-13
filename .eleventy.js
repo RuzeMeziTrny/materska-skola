9986300a6a70ef2df041bea0248138fc086be0df
Ivana Ruzickova <ruzemezitrny@gmail.com>
2021-02-13 11:54:33 +0100

    starting files + header and footer

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+module.exports = function(eleventyConfig) {
+  // Výchozí výstupní složka: _site
+  // Zkopírovat `images/` do `_site/images`
+  eleventyConfig.addPassthroughCopy("images");
+  // Zkopírovat `css/` to `_site/css/`
+  eleventyConfig.addPassthroughCopy("css");
+};
