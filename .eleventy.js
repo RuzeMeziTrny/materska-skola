de112e50779d931f46b5f994149e0e996f92e103
Ivana Ruzickova <ruzemezitrny@gmail.com>
2021-02-14 17:40:32 +0100

    navbar with dropdowns on hover

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Výchozí výstupní složka: _site
   // Zkopírovat `images/` do `_site/images`
-  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy('images');
   // Zkopírovat `css/` to `_site/css/`
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy('css');
+  // Zkopírovat `js/` to `_site/js/`
+  eleventyConfig.addPassthroughCopy('js');
 };
