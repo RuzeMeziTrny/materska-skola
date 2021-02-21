0af299607ba8878df70f7cf55f2b2ffbb172c94c
Ivana Ruzickova <ruzemezitrny@gmail.com>
2021-02-21 12:24:45 +0100

    svp section

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('images');
   // Zkopírovat `css/` to `_site/css/`
   eleventyConfig.addPassthroughCopy('css');
+  // Zkopírovat `docs/` to `_site/docs/`
+  eleventyConfig.addPassthroughCopy('docs');
   // Zkopírovat `js/` to `_site/js/`
   eleventyConfig.addPassthroughCopy('js');
 };
